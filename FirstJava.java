3588a2d39bd6f0cbd26f122904b0aa67d0c4884f
patrickmbasco <59862023+patrickmbasco@users.noreply.github.com>
2020-04-06 16:03:14 -0700

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,9 @@
+
+public class FirstJava {
+    public static void main(String[] args){
+        System.out.println("My name is Patrick Basco");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is American Canyon, CA");
+    }
+
+}
